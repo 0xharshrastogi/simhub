e0a8b0bf65a7ea423514cb925b2df4248c91add0
Harsh Rastogi <rastogiharsh04@gmail.com>
2023-04-10 16:34:12 +0530

    add simcompanies api proxy to frontend

--- frontend/simhubui/src/main.tsx
+++ frontend/simhubui/src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RealmType, SimCompanies } from "simcompanies";
 import { App } from "./app/App";
 import "./index.css";
 
@@ -8,3 +9,6 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 		<App />
 	</React.StrictMode>
 );
+
+const realm = SimCompanies.loadRealm(RealmType.Magnates);
+console.log(realm.fetchResourceList().then(console.log).catch(console.log));
